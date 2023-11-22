1d03288cce57304764bf4b8903cbb8e5d6f64009
rere2133 <reckie2133@gmail.com>
2023-11-22 16:26:51 +0800

    add vue-router4

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { registerPlugins } from '@/plugins'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+registerPlugins(app)
+
+app.mount('#app')
